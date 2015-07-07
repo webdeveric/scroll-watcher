8c806b41d2c88de5b45116876d8ce20a62575ebf
Eric King <eric.king@lonelyplanet.com>
2015-07-07 10:17:12 -0500

    Updated functions to be bound to window

--- src/animationFrame.js
+++ src/animationFrame.js
@@ -1,36 +1,36 @@
 const prefix = [ 'webkit', 'moz', 'ms', 'o' ];
 
-export const requestAnimationFrame = (( w ) => {
+export const requestAnimationFrame = (() => {
 
-  for ( let i = 0, limit = prefix.length ; i < limit && !w.requestAnimationFrame ; ++i ) {
-    w.requestAnimationFrame = w[ prefix[ i ] + 'RequestAnimationFrame' ];
+  for ( let i = 0, limit = prefix.length ; i < limit && !window.requestAnimationFrame ; ++i ) {
+    window.requestAnimationFrame = window[ prefix[ i ] + 'RequestAnimationFrame' ];
   }
 
-  if ( ! w.requestAnimationFrame ) {
+  if ( ! window.requestAnimationFrame ) {
     let lastTime = 0;
-    w.requestAnimationFrame = ( callback ) => {
+    window.requestAnimationFrame = ( callback ) => {
       const now   = new Date().getTime(),
             ttc   = Math.max( 0, 16 - now - lastTime ),
-            timer = w.setTimeout( () => { callback( now + ttc ); }, ttc );
+            timer = window.setTimeout( () => { callback( now + ttc ); }, ttc );
       lastTime = now + ttc;
       return timer;
     };
   }
 
-  return w.requestAnimationFrame;
-}( window ));
+  return window.requestAnimationFrame.bind( window );
+}());
 
-export const cancelAnimationFrame = (( w ) => {
+export const cancelAnimationFrame = (() => {
 
-  for ( let i = 0, limit = prefix.length ; i < limit && !w.cancelAnimationFrame ; ++i ) {
-    w.cancelAnimationFrame  = w[ prefix[ i ] + 'CancelAnimationFrame' ] || w[ prefix[ i ] + 'CancelRequestAnimationFrame' ];
+  for ( let i = 0, limit = prefix.length ; i < limit && !window.cancelAnimationFrame ; ++i ) {
+    window.cancelAnimationFrame  = window[ prefix[ i ] + 'CancelAnimationFrame' ] || window[ prefix[ i ] + 'CancelRequestAnimationFrame' ];
   }
 
-  if ( ! w.cancelAnimationFrame ) {
-    w.cancelAnimationFrame = ( timer ) => {
-      w.clearTimeout( timer );
+  if ( ! window.cancelAnimationFrame ) {
+    window.cancelAnimationFrame = ( timer ) => {
+      window.clearTimeout( timer );
     };
   }
 
-  return w.cancelAnimationFrame;
-}( window ));
+  return window.cancelAnimationFrame.bind( window );
+}());
